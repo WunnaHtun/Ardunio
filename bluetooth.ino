2bbbe2f8fac92e489133869b78e953704faae203
WunnaHtun <dreamangle199@gmail.com>
2018-06-03 18:24:55 +0630

    bluetooth
    bluetooth code for ardunio

---
+++ bluetooth.ino
@@ -0,0 +1,28 @@
+/*
+* Bluetooh Basic: LED ON OFF - Avishkar
+* Coder - Mayoogh Girish
+* Website - http://bit.do/Avishkar
+* Download the App : https://github.com/Mayoogh/Arduino-Bluetooth-Basic
+* This program lets you to control a LED on pin 13 of arduino using a bluetooth module
+*/
+char data = 0;            //Variable for storing received data
+void setup()
+{
+    Serial.begin(9600);   //Sets the baud for serial data transmission                               
+    pinMode(13, OUTPUT);  //Sets digital pin 13 as output pin
+}
+void loop()
+{
+   if(Serial.available() > 0)      // Send data only when you receive data:
+   {
+      data = Serial.read();        //Read the incoming data & store into data
+      Serial.print(data);          //Print Value inside data in Serial monitor
+      Serial.print("\n");        
+      if(data == 'r')     {         // Checks whether value of data is equal to 1
+         digitalWrite(13, HIGH);   //If value is 1 then LED turns ON
+          digitalWrite(12, LOW);}   //If value is 1 then LED turns ON
+      else if(data == 'y')       {  //  Checks whether value of data is equal to 0
+         digitalWrite(13, LOW);    //If value is 0 then LED turns OFF
+          digitalWrite(12, HIGH);   //If value is 1 then LED turns ON
+      }}
+}
